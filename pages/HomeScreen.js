690b4d070c9f8314380a23f07947e1e33c73c1e1
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-04-16 20:18:32 +0530

    Added FAB in home screen

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,11 +1,80 @@
-import { View } from "react-native"
-import { styles } from "../styles.js"
+import { ActionSheetIOS, Alert, Platform, View } from "react-native"
+import { styles, theme } from "../styles.js"
 import Toolbar from "../components/Toolbar.js"
+import { FAB } from "@rneui/themed"
+import { BottomSheet, ListItem } from "@rneui/themed"
+import { useState } from "react"
 
 export default function HomeScreen() {
+	const [isVisible, setIsVisible] = useState(false)
+	const fabActions = ["Scan QR code", "Enter Manually", "Cancel"]
+
+	const androidList = [
+		{ title: fabActions[0], onPress: () => setResult(0) },
+		{ title: fabActions[1], onPress: () => setResult(1) },
+		{
+			title: fabActions[2],
+			containerStyle: { backgroundColor: "#ff0000" },
+			titleStyle: { color: "white" },
+			onPress: () => setResult(2),
+		},
+	]
+
+	const handleFabClick = () => {
+		if (Platform.OS == "ios") {
+			ActionSheetIOS.showActionSheetWithOptions(
+				{
+					options: [...fabActions],
+					cancelButtonIndex: 2,
+					userInterfaceStyle: "dark",
+				},
+				setResult
+			)
+		} else if (Platform.OS == "android") {
+			setIsVisible(true)
+		}
+	}
+
+	const setResult = (idx) => {
+		setIsVisible(false)
+		switch (idx) {
+			case 0:
+				Alert.alert("Scan QR code")
+				break
+			case 1:
+				Alert.alert("Manual")
+				break
+		}
+	}
+
 	return (
 		<View style={styles.container}>
 			<Toolbar title="Tokky" />
+
+			<BottomSheet
+				modalProps={{}}
+				isVisible={isVisible}
+				onBackdropPress={() => setIsVisible(false)}
+			>
+				{androidList.map((item, i) => (
+					<ListItem
+						key={i}
+						containerStyle={item.containerStyle}
+						onPress={item.onPress}
+					>
+						<ListItem.Title style={item.titleStyle}>
+							{item.title}
+						</ListItem.Title>
+					</ListItem>
+				))}
+			</BottomSheet>
+
+			<FAB
+				onPress={handleFabClick}
+				placement="right"
+				icon={{ name: "add", color: "white" }}
+				color={theme.primary_color}
+			/>
 		</View>
 	)
 }
