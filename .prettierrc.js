7c372aff0440226cdb25bdfba944d88c16c73b4f
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-05-02 22:45:32 +0530

    Removed expo and moved to react-native cli

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    semi: false,
+}
--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
-import { StyleSheet } from 'react-native'
+import { StatusBar, StyleSheet } from 'react-native'
 import { Provider } from 'react-redux'
 import useTheme, { appTheme } from './src/Theming'
 import SafeArea from './src/components/SafeArea'
@@ -14,71 +13,71 @@ import HomeScreen from './src/pages/HomeScreen'
 import NewAccountScreen from './src/pages/NewAccountScreen'
 
 export type RootStackParamList = {
-	HomeScreen: undefined
-	NewAccountScreen: undefined
-	EditAccountScreen: { account: Account }
+    HomeScreen: undefined
+    NewAccountScreen: undefined
+    EditAccountScreen: { account: Account }
 }
 
 export default function App() {
-	const Stack = createNativeStackNavigator<RootStackParamList>()
+    const Stack = createNativeStackNavigator<RootStackParamList>()
 
-	const theme = useTheme()
-	const styles = appStyles(theme)
+    const theme = useTheme()
+    const styles = appStyles(theme)
 
-	useEffect(() => {
-		AccountsDB.getInstance().init()
-	}, [])
+    useEffect(() => {
+        AccountsDB.getInstance().init()
+    }, [])
 
-	return (
-		<Provider store={store}>
-			<SafeArea>
-				<StatusBar style="auto" />
-				<NavigationContainer>
-					<Stack.Navigator>
-						<Stack.Screen
-							name="HomeScreen"
-							component={HomeScreen}
-							options={{
-								title: 'Tokky',
-								headerStyle: styles.headerStyle,
-								headerTitleStyle: styles.headerTitleStyle,
-								headerTintColor: theme.color.primary_color,
-								headerLargeTitle: true,
-							}}
-						/>
-						<Stack.Screen
-							name="NewAccountScreen"
-							component={NewAccountScreen}
-							options={{
-								headerTitle: 'New Account',
-								headerStyle: styles.headerStyle,
-								headerTitleStyle: styles.headerTitleStyle,
-								headerTintColor: theme.color.primary_color,
-							}}
-						/>
-						<Stack.Screen
-							name="EditAccountScreen"
-							component={EditAccountScreen}
-							options={{
-								headerTitle: 'Update account',
-								headerStyle: styles.headerStyle,
-								headerTitleStyle: styles.headerTitleStyle,
-								headerTintColor: theme.color.primary_color,
-							}}
-						/>
-					</Stack.Navigator>
-				</NavigationContainer>
-			</SafeArea>
-		</Provider>
-	)
+    return (
+        <Provider store={store}>
+            <SafeArea>
+                <StatusBar />
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="HomeScreen"
+                            component={HomeScreen}
+                            options={{
+                                title: 'Tokky',
+                                headerStyle: styles.headerStyle,
+                                headerTitleStyle: styles.headerTitleStyle,
+                                headerTintColor: theme.color.primary_color,
+                                headerLargeTitle: true,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="NewAccountScreen"
+                            component={NewAccountScreen}
+                            options={{
+                                headerTitle: 'New Account',
+                                headerStyle: styles.headerStyle,
+                                headerTitleStyle: styles.headerTitleStyle,
+                                headerTintColor: theme.color.primary_color,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="EditAccountScreen"
+                            component={EditAccountScreen}
+                            options={{
+                                headerTitle: 'Update account',
+                                headerStyle: styles.headerStyle,
+                                headerTitleStyle: styles.headerTitleStyle,
+                                headerTintColor: theme.color.primary_color,
+                            }}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </SafeArea>
+        </Provider>
+    )
 }
 
 const appStyles = (theme: typeof appTheme) =>
-	StyleSheet.create({
-		headerStyle: {
-			backgroundColor: theme.color.bg,
-		},
-		headerTitleStyle: {
-			color: theme.color.text_color_primary,
-		},
-	})
+    StyleSheet.create({
+        headerStyle: {
+            backgroundColor: theme.color.bg,
+        },
+        headerTitleStyle: {
+            color: theme.color.text_color_primary,
+        },
+    })
